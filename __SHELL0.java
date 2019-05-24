d482ce8803f0f4146d55f0ad588630854a14b951
vinayak bhat <viniru100@gmail.com>
2019-05-25 00:46:44 +0530

    complete

---
+++ __SHELL0.java
@@ -0,0 +1,10 @@
+
+public class __SHELL0 extends bluej.runtime.Shell {
+
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+management.main(__bluej_param0);
+
+
+}}
